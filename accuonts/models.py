d1fc54af1a5a96fd84d3d56fda3e84a63bda3f03
elixez9 <a1381ahmadi@gmail.com>
2025-03-10 17:03:06 +0330

    v1

---
+++ accuonts/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    phon_number = models.CharField(max_length=12, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)  #برای اینکه مشخص بشه کاربر وارد شده ادمین هست
+
+    def __str__(self):
+        return self.email
+
+    def USERNAME_FIELD(self):
+        return self.phon_number
+
+    def REQUIRED_FIELDS(self):
+        return ['emaile']
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perm(self, perm, obj=None):
+        return True
+
+
+    def is_staff(self):
+        return self.is_admin
