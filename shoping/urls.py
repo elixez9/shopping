4cb414d431c3c3045e816939e33c89ad4224597b
elixez9 <a1381ahmadi@gmail.com>
2025-08-05 19:26:02 +0330

    Fix the problem of not displaying photos

--- shoping/urls.py
+++ shoping/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+
+from shoping import settings
 
 urlpatterns = [
     path('', include('home.urls')),
     path('accounts/', include('accuonts.urls')),
     path('admin/', admin.site.urls),
 ]
+###Fix the problem of not displaying photos
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
